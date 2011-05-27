ee60fb6c87587a403152eda205a21ba652847d28
Alasdair <alasdair@ubuntu.ubuntu-domain>
2011-05-27 07:42:20 +0100

    Never save the original file.

--- lastfmImages.js
+++ lastfmImages.js
@@ -11,7 +11,7 @@ var lastfm = new lfm_api.LastFM({
 	apiSecret : 'aff4890d7cb9492bc72250abbeffc3e1'
 });
 
-function getAlbumImageFromLastFm(artist, album, callback) {
+function getAlbumImageUrlFromLastFm(artist, album, callback) {
    	lastfm.album.getInfo({artist: artist, album: album}, {success: function(data){
         var lastfmImageUrl;
 		if(data && data.album && data.album.image) {
@@ -49,39 +49,30 @@ function sendImage(response, imagePath) {
     }
 };
 
-function downloadImage(imageUrl, filename, callback) {
+function downloadImage(imageUrl, callback) {
     requestLib({uri: imageUrl, encoding: "binary"}, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            fs.writeFile(filename, body, "binary", function(err) {
-                callback(true);
-            });
+            callback(body);
         } else {
-            callback(false);
+            callback(undefined);
         }
     });
 }
 
 function getOriginalSizeImage(albumId, artist, album, callback) {
-    var albumFile = getImageFilename(undefined, albumId, "original");
-    path.exists(albumFile, function(exists) {
-        if(exists) {
-            callback(albumFile);
-        } else {
-            getAlbumImageFromLastFm(artist, album, function(imageUrl) {
-                if(imageUrl) {
-                    downloadImage(imageUrl, albumFile, function(result) {
-                        callback(albumFile);
-                    });
-                } else {
-                    callback(undefined);
-                }
+    getAlbumImageUrlFromLastFm(artist, album, function(imageUrl) {
+        if(imageUrl) {
+            downloadImage(imageUrl, function(fileData) {
+                callback(fileData);
             });
+        } else {
+            callback(undefined);
         }
     });
 }
 
-function resizeImage(oldFile, newFile, size, callback) {
-    imagemagick.resize({srcPath: oldFile, dstPath: newFile, width: size}, function(err, stdout, stderr) {
+function resizeImage(fileData, newFile, size, callback) {
+    imagemagick.resize({srcData: fileData, dstPath: newFile, width: size}, function(err, stdout, stderr) {
         if(err) {
             console.log(err);
             callback(false);
@@ -97,9 +88,9 @@ function sendAlbumImage(response, albumId, artist, album, size, failureCallback)
         if(exists) {
             sendImage(response, resizedAlbumFile);
         } else {
-            getOriginalSizeImage(albumId, artist, album, function(originalAlbumFile) {
-                if(originalAlbumFile) {
-                    resizeImage(originalAlbumFile, resizedAlbumFile, size, function(success) {
+            getOriginalSizeImage(albumId, artist, album, function(fileData) {
+                if(fileData) {
+                    resizeImage(fileData, resizedAlbumFile, size, function(success) {
                         if(success) {
                             sendImage(response, resizedAlbumFile);
                         } else {
@@ -118,10 +109,13 @@ function getImage(request, response, size) {
     var urlObj = url.parse(request.url, true);
 		
     //TODO: Check parameters
+    //TODO: Make directories if they're missing
     //TODO: Get track image if no album
     //TODO: Deal properly with getting no response from Last.fm (we don't want to keep going back to them).
     //TODO: Don't even save the original
+    //TODO: Make sure we're not doing more than one request at a time for the same track
     //TODO: Make it all nice
+    //TODO: Only download medium images if we can get away with it
 	db.addTrack(urlObj.query, function (trackId, artistId, albumId) {
         sendAlbumImage(response, albumId, urlObj.query["artist"], urlObj.query["album"], size, function() {
         });
