d75dd5a17a22acac7db01b9264f7a63b33bb3a0a
Alasdair North <alnorth@gmail.com>
2011-06-07 23:13:45 +0100

    Add top level error handler.

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 var http = require('http');
 http.createServer(require('./lrdata').urls).listen(8080);
 console.log("Server running");
+
+process.on('uncaughtException',function(error){
+    console.log(error);
+})
